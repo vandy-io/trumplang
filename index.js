c11383ade457be442dee641e3a4daa5861de611b
Matt Vandy <122590105+vandy-io@users.noreply.github.com>
2025-01-07 15:18:00 -0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,43 @@
+// index.js
+
+const Trumplang = {
+    BelieveMe: (output) => console.log(output),
+
+    Tremendous: (condition, tremendousBlock, disasterBlock) => {
+        if (condition) {
+            tremendousBlock();
+        } else if (disasterBlock) {
+            disasterBlock();
+        }
+    },
+
+    HuuugeLoop: (times, action) => {
+        for (let i = 0; i < times; i++) {
+            action();
+        }
+    },
+
+    FakeNumber: (value) => value * 1.1, // Inflates numbers by 10%
+
+    Covfefe: undefined,
+
+    Bigly: (value) => (value ? "TrueBigly" : "FakeNews"),
+
+    Try: (tryBlock, catchBlock) => {
+        try {
+            tryBlock();
+        } catch (error) {
+            catchBlock(error);
+        }
+    },
+
+    BuildTheWall: (data) => data.filter((item) => item.allowed),
+
+    Tweet: (message) => console.log(`[TWEET]: ${message}`),
+
+    AlternativeFacts: (data) => JSON.stringify(data) + " (but better!)",
+
+    InstallDependency: (dependency) => console.log(`Installing ${dependency} (only the greatest)`),
+};
+
+module.exports = Trumplang;
